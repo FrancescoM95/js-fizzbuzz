f682d8e9e9e1378d98abd5ed0199c624dab3612c
FrancescoM95 <mon95fran@gmail.com>
2023-12-20 15:09:00 +0100

    step 2

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@ console.log('JS OK');
 //* 1. Creo un ciclo che stampa in console numeri da `1` a `100`.
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    //* 2. SE il numero è multiplo sia di `3` che di `5`, viene stampato in console “FizzBuzz”.
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    }
 }
