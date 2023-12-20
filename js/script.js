11f808320fa9e0f93d4ab0ee914d8b1f051e2bef
FrancescoM95 <mon95fran@gmail.com>
2023-12-20 15:19:57 +0100

    step 3 & 5

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ console.log('JS OK');
 
 for (let i = 1; i <= 100; i++) {
 
-    //* 2. SE il numero è multiplo sia di `3` che di `5`, viene stampato in console “FizzBuzz”.
-
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {  //* 2. SE il numero è multiplo sia di `3` che di `5`, viene stampato in console “FizzBuzz”.
         console.log("FizzBuzz");
-    } else {
+
+    } else if (i % 3 === 0) {          //* 3. OPPURE SE il numero è solo multiplo di `3`, viene stampato in console “Fizz”.
+        console.log("Fizz");
+
+    } else {                          //* 5. OPPURE viene stampato solo il numero.
         console.log(i);
     }
 }
