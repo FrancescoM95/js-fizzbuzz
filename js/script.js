7a106035fc5a4a866b6e357a74b664c4e68c6c6e
FrancescoM95 <mon95fran@gmail.com>
2023-12-20 16:22:26 +0100

    bonus1 step 3&4&5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 console.log('JS OK');
 
-//# 1. Richiamo il contenitore nel DOM
+//# 1. Richiamo il contenitore nel DOM.
 const container = document.getElementById('row');
 
 //* 1. Creo un ciclo che stampa in console numeri da `1` a `100`.
 
 for (let i = 1; i <= 100; i++) {
 
-    let value;                         //# 2. Creo una variabile il cui valore viene assegnato dal ciclo che generare numeri da 1 a 100
+    let value;                         //# 2. Creo una variabile il cui valore viene assegnato dal ciclo che generare numeri da 1 a 100.
 
     if (i % 3 === 0 && i % 5 === 0) {  //# 2a. SE il numero è multiplo sia di `3` che di `5`, viene dato il valore “FizzBuzz”.
         value = 'FizzBuzz';
@@ -25,4 +25,14 @@ for (let i = 1; i <= 100; i++) {
         value = i;                   //# 2d. OPPURE viene stampato solo il numero.
         console.log(value);
     }
-}
+
+    //# 3. Creo un nuovo elemento per ogni valore.
+    const col = document.createElement('div');
+    col.classList.add('col', 'col-1', 'text-center');
+
+    //# 4. Inserisco il valore nell'elemento.
+    col.append(value);
+
+    //# 5. Inserisco l'elemento nel contenitore.
+    container.appendChild(col);
+} 
